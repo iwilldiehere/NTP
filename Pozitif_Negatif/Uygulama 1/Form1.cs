7efa13a264112fd220174f7c5fd1144e384877df
iwilldiehere <shrapnelistaken43@gmail.com>
2021-12-24 11:52:18 +0300

    örnekler

---
+++ Pozitif_Negatif/Uygulama 1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int a, sayac=0, toplam = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rnd = new Random();
+
+            for (int i = 100 ; i < 300; i++)
+            {
+                a = rnd.Next(100, 200);
+                listBox1.Items.Add(a);
+                sayac++;
+                toplam = a;
+                
+            }
+            #region if'ler
+
+                
+            
+            if(toplam%3==0)
+      {
+      
+                listBox2.Items.Add(toplam);
+      } 
+      if(toplam%5==0)
+      {
+      
+          listBox4.Items.Add(toplam);
+      }
+       if(toplam%7==0)
+        {   
+            
+           listBox6.Items.Add(a);
+        }
+   if(toplam%3==0 || toplam%5==0)
+   {
+      
+       listBox3.Items.Add(a);
+   }
+   if (toplam % 3 == 0 || toplam % 7 == 0)
+   {
+  
+       listBox5.Items.Add(a);
+   }
+   if (toplam % 3 == 0 || toplam % 5 == 0)
+   {
+
+       listBox7.Items.Add(a);
+   }
+            
+            #endregion
+
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+          
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Pozitif_Negatif/Uygulama 2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                 listBox1.Items.Clear();
+            Random rnd = new Random();
+            int a;
+            for (int i = 0; i < 50; i++)
+            {
+                a = rnd.Next(-100, 100);
+                listBox1.Items.Add(a);
+             
+            }
+    }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            #region pozitif
+            
+            
+            for (int i = 0; i < 50; i++)
+            {
+                int sayi = (int)listBox1.Items[i];
+                if (radioButton1.Checked && (sayi > 0 ) && radioButton3.Checked)
+                {
+
+                  if (sayi % 3 == 0)
+                    {
+                        listBox2.Items.Add(sayi);
+                    }
+                    }
+        
+                  if (radioButton1.Checked && (sayi > 0) && radioButton4.Checked)
+                  {
+
+                      if (sayi % 5 == 0)
+                      {
+                          listBox2.Items.Add(sayi);
+                      }
+                       }
+         
+              
+            if (radioButton1.Checked && (sayi > 0) && radioButton5.Checked)
+                  {
+
+                      if (sayi % 7 == 0)
+                      {
+                          listBox2.Items.Add(sayi);
+                      }
+             }
+            #endregion
+               
+                 #region negatif
+            
+         
+     
+            {
+                int ab = (int)listBox1.Items[i];
+                if (radioButton2.Checked && (sayi < 0 ) && radioButton3.Checked)
+                {
+
+                  if (sayi % 3 == 0)
+                    {
+                        listBox2.Items.Add(ab);
+                    }
+                    }
+        
+                  if (radioButton2.Checked && (sayi < 0) && radioButton4.Checked)
+                  {
+
+                      if (sayi % 5 == 0)
+                      {
+                          listBox2.Items.Add(ab);
+                      }
+                       }
+         
+              
+            if (radioButton2.Checked && (sayi < 0) && radioButton5.Checked)
+                  {
+
+                      if (sayi % 7 == 0)
+                      {
+                          listBox2.Items.Add(ab);
+                      }
+             }
+         
+                #endregion
+            }
+
+
+
+            }      
+    }
+
+    }
+}
