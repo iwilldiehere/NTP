6675166a2fd192011e315c319b473cda3896b90c
iwilldiehere <91111883+iwilldiehere@users.noreply.github.com>
2022-03-23 09:18:25 +0200

    pizza otomasyon

---
+++ Access ile form veritabanı bağlama/Pizza Otomasyon/pizzaOtomasyonu/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Pizzacı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbPizzaBoy = new System.Windows.Forms.ComboBox();
+            this.cmbİçecek = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtBarkodNO = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(17, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adı Soyadı:";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(97, 47);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(129, 20);
+            this.txtAdSoyad.TabIndex = 2;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(371, 40);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(129, 20);
+            this.txtTelefon.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(291, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Telefon:";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(97, 189);
+            this.txtAdres.Multiline = true;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(129, 78);
+            this.txtAdres.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(17, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Adres:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(522, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Pizza Boy:";
+            // 
+            // cmbPizzaBoy
+            // 
+            this.cmbPizzaBoy.FormattingEnabled = true;
+            this.cmbPizzaBoy.Items.AddRange(new object[] {
+            "Küçük ",
+            "Orta",
+            "Büyük"});
+            this.cmbPizzaBoy.Location = new System.Drawing.Point(602, 45);
+            this.cmbPizzaBoy.Name = "cmbPizzaBoy";
+            this.cmbPizzaBoy.Size = new System.Drawing.Size(129, 21);
+            this.cmbPizzaBoy.TabIndex = 8;
+            // 
+            // cmbİçecek
+            // 
+            this.cmbİçecek.FormattingEnabled = true;
+            this.cmbİçecek.Items.AddRange(new object[] {
+            "Su ",
+            "Kola",
+            "Fanta",
+            "Ayran",
+            "Gazoz ",
+            "Şalgam"});
+            this.cmbİçecek.Location = new System.Drawing.Point(602, 82);
+            this.cmbİçecek.Name = "cmbİçecek";
+            this.cmbİçecek.Size = new System.Drawing.Size(129, 21);
+            this.cmbİçecek.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(522, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "İçecek:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(97, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 37);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Sipariş Al";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(707, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 37);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(232, 168);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(458, 185);
+            this.dataGridView1.TabIndex = 28;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(707, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 43);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtBarkodNO
+            // 
+            this.txtBarkodNO.Location = new System.Drawing.Point(97, 87);
+            this.txtBarkodNO.Name = "txtBarkodNO";
+            this.txtBarkodNO.Size = new System.Drawing.Size(129, 20);
+            this.txtBarkodNO.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(17, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BarkodNO :";
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Location = new System.Drawing.Point(707, 262);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(113, 38);
+            this.btnGüncelle.TabIndex = 30;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(935, 368);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbİçecek);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbPizzaBoy);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBarkodNO);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbPizzaBoy;
+        private System.Windows.Forms.ComboBox cmbİçecek;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtBarkodNO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGüncelle;
+    }
+}
+
---
+++ Access ile form veritabanı bağlama/Pizza Otomasyon/pizzaOtomasyonu/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace Pizzacı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglanti;
+        OleDbDataAdapter adtr;
+        DataTable dt;
+        OleDbCommand command;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            
+        }
+        private void datadoldur()
+        {
+            baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Pizzacı.accdb");
+            adtr = new OleDbDataAdapter("select * from Bilgiler", baglanti);
+            dt = new DataTable();
+            baglanti.Open();
+            adtr.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            datadoldur();
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           baglanti.Open();
+           OleDbCommand komut = new OleDbCommand("insert into Bilgiler(adsoyad,telefon,adres,pizzaboy,icecek,barkodno) values (@adsoyad,@telefon,@adres,@pizzaboy,@icecek,@barkodno)", baglanti);
+            komut.Parameters.AddWithValue("@adsoyad",txtAdSoyad.Text);
+            komut.Parameters.AddWithValue("@telefon", txtTelefon.Text);
+            komut.Parameters.AddWithValue("@adres", txtAdres.Text);
+            komut.Parameters.AddWithValue("@pizzaboy", cmbPizzaBoy.Text);
+            komut.Parameters.AddWithValue("@icecek", cmbİçecek.Text);
+            komut.Parameters.AddWithValue("@barkodno", txtBarkodNO.Text);
+            komut.ExecuteNonQuery();
+
+       
+        baglanti.Close();
+        datadoldur();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtAdSoyad.Clear();
+            txtTelefon.Clear();
+            txtAdres.Clear();
+            txtBarkodNO.Clear();
+            cmbPizzaBoy.Text = "";
+            cmbİçecek.Text = "";
+
+          
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            {
+                DialogResult dialog;
+                dialog = MessageBox.Show("Bu kayıtı silmek istiyor musunuz?", "Sil", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (dialog == DialogResult.Yes)
+                {
+                    baglanti.Open();
+                    OleDbCommand komut = new OleDbCommand("delete from Bilgiler where barkodno=@barkodno", baglanti);
+                    komut.Parameters.AddWithValue("barkodno", dataGridView1.CurrentRow.Cells["barkodno"].Value.ToString());
+                    komut.ExecuteNonQuery();
+                    baglanti.Close();
+                    DialogResult dia;
+                    dia = MessageBox.Show("Silme işlemi başarılı", "Silindi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    datadoldur();
+                    foreach (Control item in Controls)
+                    {
+                        if (item is TextBox)
+                        {
+                            item.Text = "";
+                            cmbİçecek.Text = "";
+                            cmbPizzaBoy.Text = "";
+                        }   
+                    }
+
+
+
+
+
+                }
+
+
+            }
+
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+          txtAdSoyad.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtTelefon.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtAdres.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            cmbPizzaBoy.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            cmbİçecek.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            txtBarkodNO.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+
+          
+
+
+        }
+
+        private void btnGüncelle_Click(object sender, EventArgs e)
+        {
+            
+           command = new OleDbCommand();
+            baglanti.Open();
+            command.Connection = baglanti;
+            command.CommandText = " update  Bilgiler set adsoyad='" + txtAdSoyad.Text + "', telefon='" + txtTelefon.Text + "',pizzaboy='" + cmbPizzaBoy.Text + "' ,icecek='" + cmbİçecek.Text + "' where barkodno='"+ txtBarkodNO.Text + "'   ";
+            command.ExecuteNonQuery();
+            baglanti.Close();
+            datadoldur();
+            baglanti.Close();
+        }
+    }
+}
